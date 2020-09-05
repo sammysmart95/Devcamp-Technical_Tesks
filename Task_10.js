c6db6b934ee76eb1d21287fad23555597da001e5
?ranky <sammysmart95@gmail.com>
2020-09-05 10:05:29 +0100

    add comments to my solution

--- Task_10.js
+++ Task_10.js
@@ -10,13 +10,20 @@
 const countCharacter = (str = "") => {
   let currentCount = 0;
   let result = "";
+  // iterate through the string
   for (var i = 0; i < str.length; i++) {
+    // this line uses a regular expression to match the length of each  letter globlally
+
     let count = (str.match(new RegExp(str[i], "g")) || []).length;
+    // if a letter is greater than the current count
     if (count > currentCount) {
+      // current count is assign to count
       currentCount = count;
+      // the highest count is assign to result
       result = str[i];
     }
   }
+  // the highest occuring letter is returned
   return result;
 };
 
--- Task_6.js
+++ Task_6.js
@@ -7,16 +7,23 @@
 // Input: [1, 2, 3, 4, 5, 6], 6
 // Output: [1, 2, 3]
 
+// this iitialize an array and the final number
 const sumValuesTo = (arr = [], sum = 0) => {
+  // this loop through the array to find the first number
   for (var i = 0; i < arr.length; i++) {
+    // this loop through the array to find the second number
     for (var j = i + 1; j < arr.length; j++) {
+      // this loop through the array to find the third number
       for (var k = j + 1; k < arr.length; k++) {
+        // if the three numbers is = to the given total
+        // return the numbers in an array
         if (arr[i] + arr[j] + arr[k] === sum) {
           return [arr[i], arr[j], arr[k]];
         }
       }
     }
   }
+  // return if no three numbers sum to get the total
   return -1;
 };
 console.log(sumValuesTo([1, 2, 3, 4, 5, 6], 6));
